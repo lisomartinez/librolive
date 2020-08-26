815b73401a8f379f9e6d1f7e66cd9ac20533900e
Lisandro Martinez <lisandromartinez@gmail.com>
2020-08-26 11:12:06 -0300

    Initial Commit

---
+++ src/main/java/com/github/lmartinez84/librolive/LibroliveApplication.java
@@ -0,0 +1,13 @@
+package com.github.lmartinez84.librolive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibroliveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibroliveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/lmartinez84/librolive/LibroliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lmartinez84.librolive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibroliveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
